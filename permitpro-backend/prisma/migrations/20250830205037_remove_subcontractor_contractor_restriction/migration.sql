a0a1a34b4d2e368cda8a07fc5906d60ddabaa943
Sean Swonger <sdswonger@icloud.com>
2025-08-31 17:18:42 -0400

    Clean up permitpro-backend directory - using root directory for Railway deployment

--- permitpro-backend/prisma/migrations/20250830205037_remove_subcontractor_contractor_restriction/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[companyName,licenseNumber]` on the table `Subcontractor` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Subcontractor_companyName_licenseNumber_key" ON "Subcontractor"("companyName", "licenseNumber");
